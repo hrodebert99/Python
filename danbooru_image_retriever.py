d000878cae14906b2847555e82b9f8797d8b2e2c
hrodebert99 <hrodebert99@gmail.com>
2025-04-13 15:09:09 +0800

    Update danbooru_image_retriever.py

--- danbooru_image_retriever.py
+++ danbooru_image_retriever.py
@@ -1,29 +1,71 @@
+import datetime
 import json
 import os
 import requests
 import time
+
+def get_images(page, tags):
+    response = requests.get(f'https://danbooru.donmai.us/posts.json?page={page}&tags={tags}')
+    return json.loads(response.text)
+
+def download_image(url, name = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')):
+    response = requests.get(url)
+    image = response.content
+    
+    with open(name, 'wb') as file:
+        file.write(image)
+
 page = 1
-tags = 'hatsune_miku'
+tags = 'pallas_(arknights)'
+
+print('Process started.')
+
 os.mkdir(tags)
-response = requests.get(f'https://danbooru.donmai.us/posts.json?page={page}&tags={tags}')
-images = json.loads(response.text)
+
+images = get_images(page, tags)
+
+tag_string_count = {}
+
 while not images == []:
+    print(f'    Page {page}')
+
     for image in images:
         file_ext = image['file_ext']
+        
         if file_ext in ['mp4']:
             continue
-        md5 = image['md5']
+        
+        filename = image['md5'] if 'md5' in image else datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
+        
+        tag_string = image['tag_string'].split(' ')
+        
+        for tag in tag_string:
+            tag_string_count[tag] = tag_string_count.get(tag, 0) + 1
+        
         tag_string = image['tag_string'].replace(' ', ', ')
-        with open(f'{tags}\\{md5}.txt', 'w') as file:
+        
+        with open(f'{tags}\\{filename}.txt', 'w') as file:
             file.write(tag_string)
+
+        if not 'variants' in image['media_asset']:
+            continue
+        
         for variant in image['media_asset']['variants']:
             if variant['type'] == 'original':
                 image_url = variant['url']
-        response = requests.get(image_url)
-        image = response.content
-        with open(f'{tags}\\{md5}.{file_ext}', 'wb') as file:
-            file.write(image)
+        
+        download_image(image_url, f'{tags}\\{filename}.{file_ext}')
+    
     time.sleep(1)
+    
     page += 1
-    response = requests.get(f'https://danbooru.donmai.us/posts.json?page={page}&tags={tags}')
-    images = json.loads(response.text)
+    images = get_images(page, tags)
+
+tag_string_count = sorted(tag_string_count.items(), key = lambda item: item[1], reverse = True)
+tag_string = [key for key, value in tag_string_count]
+tag_string = ', '.join(tag_string)
+
+with open(f'{tags}/_tags.txt', 'w') as file:
+    file.write(tag_string)
+
+print('Process completed.')
