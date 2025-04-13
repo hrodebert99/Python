fab10244707b81698b3a3eede5083d2017d1133c
hrodebert99 <hrodebert99@gmail.com>
2025-04-13 19:47:29 +0800

    Update danbooru_image_retriever.py

--- danbooru_image_retriever.py
+++ danbooru_image_retriever.py
@@ -3,8 +3,8 @@ import os
 import requests
 
 class Danbooru:
-    def get_posts(tags, page = 1):
-        url = f'https://danbooru.donmai.us/posts.json?tags={tags}&page={page}'
+    def get_posts(include_tags, exclude_tags = [], page = 1):
+        url = f'https://danbooru.donmai.us/posts.json?tags={include_tags}&page={page}'
         response = requests.get(url).text
         posts_json = json.loads(response)
         posts = []
@@ -15,8 +15,14 @@ class Danbooru:
             if not 'file_url' in post:
                 continue
 
-            post = DanbooruPost(post)
-            posts.append(post)
+            for tag in exclude_tags:
+                if tag in post['tag_string']:
+                    break
+            else:
+                print(post['tag_string_character'])
+
+                post = DanbooruPost(post)
+                posts.append(post)
         return posts
     
     def format_tag_string(tag_string):
@@ -56,14 +62,16 @@ class DanbooruPost:
             file.write(f'{self.tag_string_character}, {self.tag_string_copyright}, {self.tag_string_general}')
 
 if __name__ == '__main__':
-    tags = input('Tags: ')
-    save_folder = tags
+    include_tags = input('Include tags: ')
+    exclude_tags = input('Exclude tags: ')
+    exclude_tags = exclude_tags.split(' ') if not exclude_tags == '' else []
+    save_folder = include_tags
 
     print('Process started.')
     
     page = 1
     print(f'Page {page}')
-    posts = Danbooru.get_posts(tags, page)
+    posts = Danbooru.get_posts(include_tags, exclude_tags, page)
 
     while not posts == []:
         for post in posts:
@@ -72,6 +80,6 @@ if __name__ == '__main__':
             
         page += 1
         print(f'Page {page}')
-        posts = Danbooru.get_posts(tags, page)
+        posts = Danbooru.get_posts(include_tags, exclude_tags, page)
 
     print('Process completed.')
