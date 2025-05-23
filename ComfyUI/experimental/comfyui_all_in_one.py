499929a805ae677d6beeedc9f3cd87321ad1cbcc
hrodebert99 <hrodebert99@gmail.com>
2025-05-23 20:54:19 +0800

    Update comfyui_all_in_one.py

--- ComfyUI/experimental/comfyui_all_in_one.py
+++ ComfyUI/experimental/comfyui_all_in_one.py
@@ -1,9 +1,76 @@
 import datetime
 import sqlite3
-import glob
 import os
+import hashlib
 
 log_filename = f'{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.log'
+
+models_folder_path = '.\\models'
+
+exclude_files = [
+    '.\\models\\checkpoints\\put_checkpoints_here',
+    '.\\models\\clip\\put_clip_or_text_encoder_models_here',
+    '.\\models\\clip_vision\\put_clip_vision_models_here',
+    '.\\models\\configs\\anything_v3.yaml',
+    '.\\models\\configs\\v1-inference_clip_skip_2_fp16.yaml',
+    '.\\models\\configs\\v1-inference_clip_skip_2.yaml',
+    '.\\models\\configs\\v1-inference_fp16.yaml',
+    '.\\models\\configs\\v1-inference.yaml',
+    '.\\models\\configs\\v1-inpainting-inference.yaml',
+    '.\\models\\configs\\v2-inference_fp32.yaml',
+    '.\\models\\configs\\v2-inference-v_fp32.yaml',
+    '.\\models\\configs\\v2-inference-v.yaml',
+    '.\\models\\configs\\v2-inference.yaml',
+    '.\\models\\configs\\v2-inpainting-inference.yaml',
+    '.\\models\\controlnet\\put_controlnets_and_t2i_here',
+    '.\\models\\diffusers\\put_diffusers_models_here',
+    '.\\models\\diffusion_models\\put_diffusion_model_files_here',
+    '.\\models\\embeddings\\put_embeddings_or_textual_inversion_concepts_here',
+    '.\\models\\gligen\\put_gligen_models_here',
+    '.\\models\\hypernetworks\\put_hypernetworks_here',
+    '.\\models\\loras\\put_loras_here',
+    '.\\models\\photomaker\\put_photomaker_models_here',
+    '.\\models\\style_models\\put_t2i_style_model_here',
+    '.\\models\\text_encoders\\put_text_encoder_files_here',
+    '.\\models\\unet\\put_unet_files_here',
+    '.\\models\\upscale_models\\put_esrgan_and_other_upscale_models_here',
+    '.\\models\\vae\\put_vae_here',
+    '.\\models\\vae_approx\\put_taesd_encoder_pth_and_taesd_decoder_pth_here',
+    '.\\models\\vae_approx\\taef1_decoder.pth',
+    '.\\models\\vae_approx\\taef1_encoder.pth',
+    '.\\models\\vae_approx\\taesd_decoder.pth',
+    '.\\models\\vae_approx\\taesd_encoder.pth',
+    '.\\models\\vae_approx\\taesd3_decoder.pth',
+    '.\\models\\vae_approx\\taesd3_encoder.pth',
+    '.\\models\\vae_approx\\taesdxl_decoder.pth',
+    '.\\models\\vae_approx\\taesdxl_encoder.pth',
+
+    '.\\models\\clip_vision\\clip-vision_vit-h.safetensors',
+    '.\\models\\clip_vision\\OpenCLIP-ViT-bigG-14.safetensors',
+    '.\\models\\controlnet\\controlnet-union-sdxl-1.0.safetensors',
+    '.\\models\\inpaint\\fooocus_inpaint_head.pth',
+    '.\\models\\inpaint\\inpaint_v26.fooocus.patch',
+    '.\\models\\inpaint\\MAT_Places512_G_fp16.safetensors',
+    '.\\models\\ipadapter\\ip-adapter_sd15_light.bin',
+    '.\\models\\ipadapter\\ip-adapter_sd15.bin',
+    '.\\models\\ipadapter\\ip-adapter_sdxl_vit-h.bin',
+    '.\\models\\ipadapter\\ip-adapter_sdxl_vit-h.safetensors',
+    '.\\models\\ipadapter\\ip-adapter_sdxl.bin',
+    '.\\models\\ipadapter\\ip-adapter-plus_sd15.bin',
+    '.\\models\\ipadapter\\ip-adapter-plus_sdxl_vit-h.bin',
+    '.\\models\\ipadapter\\ip-adapter-plus-face_sd15.bin',
+    '.\\models\\ipadapter\\ip-adapter-plus-face_sdxl_vit-h.bin',
+    '.\\models\\sams\\sam_vit_b_01ec64.pth',
+    '.\\models\\upscale_models\\4x_NMKD-Superscale-SP_178000_G.pth',
+    '.\\models\\upscale_models\\OmniSR_X2_DIV2K.safetensors',
+    '.\\models\\upscale_models\\OmniSR_X3_DIV2K.safetensors',
+    '.\\models\\upscale_models\\OmniSR_X4_DIV2K.safetensors',
+    '.\\models\\upscale_models\\RealESRGAN_x4.pth',
+    '.\\models\\vae\\SDXL 1.0\\nucleardiffusion\\sdxl_vae.safetensors',
+
+    '.\\models\\stable_diffusion_model_organizer.py',
+    '.\\models\\model_directory_flattener.py'
+]
 
 def log(message):
     message = f'{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {message}'
@@ -81,84 +148,39 @@ def insert_default_data(connection, cursor):
 
     connection.commit()
 
-models_folder_path = '.\\models'
+def calculate_hash(file_path):
+    hash = hashlib.sha256()
 
-exclude_files = [
-    '.\\models\\checkpoints\\put_checkpoints_here',
-    '.\\models\\clip\\put_clip_or_text_encoder_models_here',
-    '.\\models\\clip_vision\\put_clip_vision_models_here',
-    '.\\models\\configs\\anything_v3.yaml',
-    '.\\models\\configs\\v1-inference_clip_skip_2_fp16.yaml',
-    '.\\models\\configs\\v1-inference_clip_skip_2.yaml',
-    '.\\models\\configs\\v1-inference_fp16.yaml',
-    '.\\models\\configs\\v1-inference.yaml',
-    '.\\models\\configs\\v1-inpainting-inference.yaml',
-    '.\\models\\configs\\v2-inference_fp32.yaml',
-    '.\\models\\configs\\v2-inference-v_fp32.yaml',
-    '.\\models\\configs\\v2-inference-v.yaml',
-    '.\\models\\configs\\v2-inference.yaml',
-    '.\\models\\configs\\v2-inpainting-inference.yaml',
-    '.\\models\\controlnet\\put_controlnets_and_t2i_here',
-    '.\\models\\diffusers\\put_diffusers_models_here',
-    '.\\models\\diffusion_models\\put_diffusion_model_files_here',
-    '.\\models\\embeddings\\put_embeddings_or_textual_inversion_concepts_here',
-    '.\\models\\gligen\\put_gligen_models_here',
-    '.\\models\\hypernetworks\\put_hypernetworks_here',
-    '.\\models\\loras\\put_loras_here',
-    '.\\models\\photomaker\\put_photomaker_models_here',
-    '.\\models\\style_models\\put_t2i_style_model_here',
-    '.\\models\\text_encoders\\put_text_encoder_files_here',
-    '.\\models\\unet\\put_unet_files_here',
-    '.\\models\\upscale_models\\put_esrgan_and_other_upscale_models_here',
-    '.\\models\\vae\\put_vae_here',
-    '.\\models\\vae_approx\\put_taesd_encoder_pth_and_taesd_decoder_pth_here',
-    '.\\models\\vae_approx\\taef1_decoder.pth',
-    '.\\models\\vae_approx\\taef1_encoder.pth',
-    '.\\models\\vae_approx\\taesd_decoder.pth',
-    '.\\models\\vae_approx\\taesd_encoder.pth',
-    '.\\models\\vae_approx\\taesd3_decoder.pth',
-    '.\\models\\vae_approx\\taesd3_encoder.pth',
-    '.\\models\\vae_approx\\taesdxl_decoder.pth',
-    '.\\models\\vae_approx\\taesdxl_encoder.pth',
+    with open(file_path, 'rb') as file:
+        for chunk in iter(lambda: file.read(4096), b''):
+            hash.update(chunk)
+    
+    return hash.hexdigest()
 
-    '.\\models\\clip_vision\\clip-vision_vit-h.safetensors',
-    '.\\models\\clip_vision\\OpenCLIP-ViT-bigG-14.safetensors',
-    '.\\models\\controlnet\\controlnet-union-sdxl-1.0.safetensors',
-    '.\\models\\inpaint\\fooocus_inpaint_head.pth',
-    '.\\models\\inpaint\\inpaint_v26.fooocus.patch',
-    '.\\models\\inpaint\\MAT_Places512_G_fp16.safetensors',
-    '.\\models\\ipadapter\\ip-adapter_sd15_light.bin',
-    '.\\models\\ipadapter\\ip-adapter_sd15.bin',
-    '.\\models\\ipadapter\\ip-adapter_sdxl_vit-h.bin',
-    '.\\models\\ipadapter\\ip-adapter_sdxl_vit-h.safetensors',
-    '.\\models\\ipadapter\\ip-adapter_sdxl.bin',
-    '.\\models\\ipadapter\\ip-adapter-plus_sd15.bin',
-    '.\\models\\ipadapter\\ip-adapter-plus_sdxl_vit-h.bin',
-    '.\\models\\ipadapter\\ip-adapter-plus-face_sd15.bin',
-    '.\\models\\ipadapter\\ip-adapter-plus-face_sdxl_vit-h.bin',
-    '.\\models\\sams\\sam_vit_b_01ec64.pth',
-    '.\\models\\upscale_models\\4x_NMKD-Superscale-SP_178000_G.pth',
-    '.\\models\\upscale_models\\OmniSR_X2_DIV2K.safetensors',
-    '.\\models\\upscale_models\\OmniSR_X3_DIV2K.safetensors',
-    '.\\models\\upscale_models\\OmniSR_X4_DIV2K.safetensors',
-    '.\\models\\upscale_models\\RealESRGAN_x4.pth',
-    '.\\models\\vae\\SDXL 1.0\\nucleardiffusion\\sdxl_vae.safetensors',
+def insert_model_to_database(connection, cursor, file_path, hash):
+    cursor.execute(f'''
+        INSERT INTO models (file_path, hash)
+        VALUES  ('{file_path}', '{hash}')
+    ''')
 
-    '.\\models\\stable_diffusion_model_organizer.py',
-    '.\\models\\model_directory_flattener.py'
-]
+    connection.commit()
+
+def process_models(connection, cursor, models_folder_path, exclude_files):
+    for root, _, files in os.walk(models_folder_path):
+        for file in files:
+            file_path = f'{root}\\{file}'
+
+            if file_path in exclude_files:
+                continue
+
+            hash = calculate_hash(file_path)
+
+            insert_model_to_database(connection, cursor, file_path, hash)
+
+            log(f'{file_path} has been added to database.')
 
 connection, cursor = connect_to_database()
 create_tables(connection, cursor)
 insert_default_data(connection, cursor)
-
-for root, folders, files in os.walk(models_folder_path):
-    for file in files:
-        file_path = f'{root}\\{file}'
-
-        if file_path in exclude_files:
-            continue
-
-        # TODO
-
+process_models(connection, cursor, models_folder_path, exclude_files)
 close_database(connection)
